2228dd1c36114bb19d6c52f6a4410d1b440d351a
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-02-27 14:36:45 -0300

    Revert "Revert "Revert "restructuring message to separate command and response"""
    This reverts commit 61e9ec609f63b8083eaa83075c053c382a0e4b44.

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,35 +1,103 @@
-mod command;
 mod error;
 mod parser;
-mod response;
 
-use error::Error;
+#[cfg(test)]
+mod tests;
 
-pub use command::IrcCommand;
-use response::IrcResponse;
+pub use error::Error;
 
-type Prefix = Option<String>;
+use self::parser::{Parameters, Prefix, Trailing};
 
 pub enum IrcMessage {
-    Command(Prefix, IrcCommand),
-    Response(IrcResponse),
-    Unknown(String),
+    Welcome {
+        realname: String,
+        servername: String,
+        nickname: String,
+        username: String,
+        hostname: String,
+    },
+    Quit {
+        message: String,
+    },
+    Privmsg {
+        sender: String,
+        target: String,
+        message: String,
+    },
 }
 
+const WELCOME_COMMAND: &str = "001";
+const QUIT_COMMAND: &str = "QUIT";
+const PRIVMSG_COMMAND: &str = "PRIVMSG";
+
 impl IrcMessage {
-    pub fn parse(message: &str) -> Result<Self, Error> {
-        let (prefix, command, parameters, trailing) = parser::parse(message)?;
+    pub fn new(content: &str) -> Result<Self, Error> {
+        let (prefix, command, parameters, trailing) = parser::parse(content)?;
 
-        if IrcCommand::is_valid_command(&command) {
-            let command = IrcCommand::new(command, parameters, trailing)?;
-            return Ok(Self::Command(prefix, command));
+        match &command[..] {
+            WELCOME_COMMAND => Self::new_welcome(prefix, parameters, trailing),
+            QUIT_COMMAND => Self::new_quit(prefix, parameters, trailing),
+            PRIVMSG_COMMAND => Self::new_privmsg(prefix, parameters, trailing),
+            _ => Err(Error::UnknownCommand(content.to_string())),
+        }
+    }
+
+    fn new_welcome(
+        _prefix: Prefix,
+        mut parameters: Parameters,
+        trailing: Trailing,
+    ) -> Result<Self, Error> {
+        let realname = parameters.pop().ok_or(Error::MissingParameter)?;
+
+        let trailing = trailing.ok_or(Error::MissingParameter)?;
+        let mut split_trailing = trailing.split_whitespace().map(str::to_owned);
+
+        let servername = split_trailing.nth(2).ok_or(Error::MissingParameter)?;
+        let nickname = split_trailing.nth(1).ok_or(Error::MissingParameter)?;
+        let mut username = split_trailing.next().ok_or(Error::MissingParameter)?;
+        let mut hostname = split_trailing.next().ok_or(Error::MissingParameter)?;
+
+        if username.is_empty() || hostname.is_empty() {
+            return Err(Error::InvalidParameter);
         }
 
-        if IrcResponse::is_valid_response(&command) {
-            let response = IrcResponse::new(command, parameters, trailing)?;
-            return Ok(Self::Response(response));
-        }
+        username.remove(0);
+        hostname.remove(0);
 
-        Ok(Self::Unknown(message.to_string()))
+        Ok(Self::Welcome {
+            realname,
+            servername,
+            nickname,
+            username,
+            hostname,
+        })
+    }
+
+    fn new_quit(
+        _prefix: Prefix,
+        _parameters: Parameters,
+        trailing: Trailing,
+    ) -> Result<Self, Error> {
+        let message = trailing.ok_or(Error::MissingParameter)?;
+
+        Ok(Self::Quit { message })
+    }
+
+    fn new_privmsg(
+        prefix: Prefix,
+        mut parameters: Parameters,
+        trailing: Trailing,
+    ) -> Result<Self, Error> {
+        parameters.reverse();
+
+        let sender = prefix.ok_or(Error::MissingParameter)?;
+        let message = trailing.ok_or(Error::MissingParameter)?;
+        let target = parameters.pop().ok_or(Error::MissingParameter)?;
+
+        Ok(Self::Privmsg {
+            sender,
+            target,
+            message,
+        })
     }
 }
