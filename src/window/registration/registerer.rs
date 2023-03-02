f32d3bef56ebff96a7a7778d38b48be0742939ab
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-03-02 16:57:38 -0300

    rename register_aux to try_register

--- src/window/registration/registerer.rs
+++ src/window/registration/registerer.rs
@@ -12,43 +12,39 @@ use super::Registration;
 impl Registration {
     pub(super) fn register(&self) {
         MainContext::default().spawn_local(clone!(@weak self as registration => async move {
-            if registration.register_aux().await.is_err() {
+            if registration.try_register().await.is_err() {
                 eprintln!("error while sending message to client");
             }
         }));
     }
 
-    async fn register_aux(&self) -> io::Result<()> {
-        if self.imp().password.enabled() {
-            self.send_pass().await?;
-        }
+    async fn try_register(&self) -> io::Result<()> {
+        self.send_pass().await?;
 
         self.send_nick().await?;
 
         self.send_user().await
     }
 
-    async fn send_user(&self) -> Result<(), io::Error> {
-        let username = self.imp().username.text();
-        let realname = self.imp().realname.text();
-        let user_command = IrcCommand::User { username, realname };
-        self.client().send(user_command).await
-    }
-
-    async fn send_nick(&self) -> Result<(), io::Error> {
-        let nickname = self.imp().nickname.text();
-        let nick_command = IrcCommand::Nick { nickname };
-        self.client().send(nick_command).await?;
-        Ok(())
-    }
-
-    async fn send_pass(&self) -> Result<(), io::Error> {
+    async fn send_pass(&self) -> io::Result<()> {
         let password = self.imp().password.text();
         let pass_command = IrcCommand::Pass { password };
 
-        self.client().send(pass_command).await?;
-        self.imp().password.disable();
+        self.client().send(pass_command).await
+    }
 
-        Ok(())
+    async fn send_nick(&self) -> io::Result<()> {
+        let nickname = self.imp().nickname.text();
+        let nick_command = IrcCommand::Nick { nickname };
+
+        self.client().send(nick_command).await
+    }
+
+    async fn send_user(&self) -> io::Result<()> {
+        let username = self.imp().username.text();
+        let realname = self.imp().realname.text();
+        let user_command = IrcCommand::User { username, realname };
+
+        self.client().send(user_command).await
     }
 }
