c13a7c508f897123a825287a0a00350f93586a5f
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-03-03 14:53:15 -0300

    remove strum, standard implementation

--- src/window/registration/field/property.rs
+++ src/window/registration/field/property.rs
@@ -1,10 +1,32 @@
 use std::ops::Deref;
-use strum_macros::EnumString;
 
-#[derive(EnumString)]
 pub enum FieldProperty {
     Name,
     Input,
     Default,
     Password,
+}
+
+impl Deref for FieldProperty {
+    type Target = str;
+    fn deref(&self) -> &str {
+        match self {
+            FieldProperty::Name => "name",
+            FieldProperty::Input => "input",
+            FieldProperty::Default => "default",
+            FieldProperty::Password => "password",
+        }
+    }
+}
+
+impl From<&str> for FieldProperty {
+    fn from(value: &str) -> Self {
+        match value {
+            "name" => FieldProperty::Name,
+            "input" => FieldProperty::Input,
+            "default" => FieldProperty::Default,
+            "password" => FieldProperty::Password,
+            _ => unimplemented!(),
+        }
+    }
 }
