d91936b8390eff90ba6214496abdd83adebc79e1
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-03-07 13:30:03 -0300

    UserPage documentation

--- src/window/session/user_page/constant.rs
+++ src/window/session/user_page/constant.rs
@@ -1,10 +1,17 @@
+//! This module contains useful constants for dealing with `UserPage`
+
 use std::ops::Deref;
 
 use gtk::glib::{ParamSpec, ParamSpecObject};
 
 use crate::client::UserData;
 
+/// All `UserPage` custom properties
+///
+/// Can be easly converted to &str to use as property names.
 pub enum UserPageProperty {
+    /// User registration data
+    /// Type: UserData
     Data,
 }
 
--- src/window/session/user_page/mod.rs
+++ src/window/session/user_page/mod.rs
@@ -1,3 +1,5 @@
+//! This module defines all [`UserPage`] related structures
+
 mod constant;
 mod imp;
 
@@ -9,6 +11,9 @@ use crate::client::UserData;
 pub use constant::UserPageProperty;
 
 glib::wrapper! {
+    /// Page to display the user information
+    /// 
+    /// Subclassifies [´gtk::Box´]
     pub struct UserPage(ObjectSubclass<imp::UserPage>)
     @extends gtk::Widget, gtk::Box,
     @implements gtk::Accessible, gtk::Buildable,
@@ -16,6 +21,7 @@ glib::wrapper! {
 }
 
 impl UserPage {
+    /// Creates a [`UserPage`] with an associated [´UserData`]
     pub fn new(data: UserData) -> Self {
         Object::builder()
             .property(&UserPageProperty::Data, data)
